b691096000100d65ab22b19e17c53d9deeb80e86
Javier Alvizures <javieralvizures2006@gmail.com>
2026-04-13 19:52:08 -0600

    fix: allow localhost:3000 and Vercel in CORS

--- server.js
+++ server.js
@@ -22,9 +22,14 @@ const PORT = process.env.PORT || 5000;
 //    MIDDLEWARES
 // ========================
 app.use(cors({
-  origin: process.env.NODE_ENV === 'production'
-    ? process.env.FRONTEND_URL
-    : ['http://localhost:3000', 'http://localhost:3001', 'http://localhost:5173', 'http://localhost:4000'],
+  origin: [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:4000',
+  'http://localhost:5173',
+  'https://jardindelosconejos.vercel.app',
+  process.env.FRONTEND_URL,
+].filter(Boolean),
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 }));

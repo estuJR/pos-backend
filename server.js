4a8380c1770cfb5dd2f2fa819411093c083b73d1
estuJR <estuardochacon2006@gmail.com>
2026-06-08 14:58:05 -0600

    fix: credentials true al CORS

--- server.js
+++ server.js
@@ -33,6 +33,7 @@ app.use(cors({
 ].filter(Boolean),
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
 }));
 
 app.use(express.json());
